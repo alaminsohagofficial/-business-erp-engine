0ce4c7cc41ce6df80025f9ce674a13eb07e33610
MD. AL AMIN SOHAG <267598403+alaminsohagofficial@users.noreply.github.com>
2026-07-06 00:13:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// সরাসরি index.html ফাইলটি রেন্ডার করার জন্য
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+// public ফোল্ডারকে স্ট্যাটিক ডিরেক্টরি হিসেবে সেট করা
+app.use(express.static(path.join(__dirname, 'public')));
+
+// যেকোনো রিকোয়েস্ট আসলেই সরাসরি index.html ফাইলটি লোড হবে
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
