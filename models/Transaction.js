6ffa4db62b223ec93fcde51be98b592ada1570c4
MD. AL AMIN SOHAG <267598403+alaminsohagofficial@users.noreply.github.com>
2026-08-04 20:14:22 +0600

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,24 +1,15 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  dealerId: { type: String, default: '3000002272' }, // SR ELECTRONICS PARK
-  dealerName: { type: String, default: 'SR ELECTRONICS PARK' },
-  companyName: {
-    type: String,
-    required: true,
-    enum: ['MINISTER_MYONE', 'BUTTERFLY_MARKETING', 'SALSABILAH_AMIN', 'SONALI_BANK_CSS']
-  },
+  dealerId: { type: String, required: true },
+  dealerName: { type: String, required: true },
+  companyName: { type: String, required: true },
   bankName: { type: String, required: true },
-  transactionType: { type: String, required: true }, // RTGS, NexusPay, NPSB, Dispute_Notice, Fund_Transfer
+  transactionType: { type: String, required: true },
   coreTrxId: { type: String, required: true, unique: true },
-  referenceLid: { type: String },
   valueDate: { type: Date, required: true },
   amount: { type: Number, required: true },
-  status: {
-    type: String,
-    enum: ['SUCCESS', 'PAID_AND_POSTED', 'DISPUTED', 'PENDING_RESOLVE'],
-    default: 'SUCCESS'
-  },
+  status: { type: String, required: true },
   narration: { type: String }
 }, { timestamps: true });
 
